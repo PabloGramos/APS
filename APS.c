565ca5c45551d6893c729298e2d7efe792bbfa34
Pablo Vinícius Gonçalves Ramos <70920611+PabloGramos@users.noreply.github.com>
2020-11-03 09:12:09 -0300

    Complementos da APS em python
    Adicionei o while em cada função e adicionei o detalhe da raiz de numero negativo

--- APS.c
+++ APS.c
@@ -1,22 +1,154 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 
-
+void inicio (void){
+	printf("------------CALCULADORA--------------\n");
+	printf("-----------Pablo-Vinicius------------\n");
+	printf("               Menu                \n");
+	printf("1 - Soma\n");
+	printf("2 - Subtracao\n");
+	printf("3 - Multiplicacao\n");
+	printf("4 - Divisao\n");
+	printf("5 - Raiz Quadrada\n");
+	printf("6 - Seno, Cosseno, Tangente\n");
+	printf("7 - Historico\n");
+	printf("0 - Sair\n\n");
+}
+void soma(){
+	int r=1,n1,n2,soma;
+	while(r>0){
+		printf("Insira o valor: ");
+		scanf("%d",&n1);
+		printf("%d + ",n1);
+		scanf("%d",&n2);
+		soma=n1+n2;
+		printf("%d + %d = %d\n\n",n1,n2,soma);
+		printf("1-Continuar 0-Sair: \n");
+		scanf("%d",&r);
+		
+	}
+}
+void sub(){
+	int r=1,n1,n2,sub;
+	while(r>0){
+		
+		
+		printf("Insira o valor: ");
+		scanf("%d",&n1);
+		printf("%d - ",n1);
+		scanf("%d",&n2);
+		sub=n1-n2;
+		printf("%d - %d = %d\n\n",n1,n2,sub);
+		printf("1-Continuar 0-Sair: \n");
+		scanf("%d",&r);
+}
+}
+void mult(){
+	int r=1,n1,n2,mult;
+	while(r>0){
+		
+		
+		printf("Insira o valor: ");
+		scanf("%d",&n1);
+		printf("%d X ",n1);
+		scanf("%d",&n2);
+		mult=n1*n2;
+		printf("%d X %d = %d\n\n",n1,n2,mult);
+		printf("1-Continuar 0-Sair: \n");
+		scanf("%d",&r);
+}
+}
+void divi(){
+	int r=1,n1,n2;
+	float divi;
+	while(r>0){
+		printf("Insira o valor: ");
+		scanf("%d",&n1);
+		printf("%d / ",n1);
+		scanf("%d",&n2);
+		if(n2==0){
+			printf("Nao existe divisao por 0!\n\n");
+			break;
+		}
+		divi=n1/n2;
+		printf("%d / %d = %.2f\n\n",n1,n2,divi);
+		printf("1-Continuar 0-Sair: \n");
+		scanf("%d",&r);
+}
+}
+void raiz(){
+	int r=1;
+	float n,raiz;
+	while(r>0){
+		printf("Insira o valor: ");
+		scanf("%f",&n);
+		if(n<0){
+			printf("Nao existe raiz de numeros negativos!\n\n");
+			break;
+		}
+		raiz=sqrt(n);
+		printf("Raiz de %.2f = %.2f\n\n",n,raiz);
+		printf("1-Continuar 0-Sair: \n");
+		scanf("%d",&r);
+}
+}
+void sct(){
+	int r=1;
+	float n,seno,cosseno,tang;
+	while(r>0){
+		printf("Insira o valor: ");
+		scanf("%f",&n);
+		seno=sin(n);
+		cosseno=cos(n);
+		tang=tan(n);
+		printf("Seno de %.2f = %.2f\n",n,seno);
+		printf("Cosseno de %.2f = %.2f\n",n,cosseno);
+		printf("Tangente de %.2f = %.2f\n\n",n,tang);
+		printf("1-Continuar 0-Sair: \n");
+		scanf("%d",&r);
+}
+}
 
 int main(){
+	int op,r=1;
+	//  DICA ADICIONAR O PONTEIRO ANTES DO WHILE PARA QUANDO FOR CHAMADO ELE ATUALIZAR UM EMBAIXO DO OUTRO
+	while(r>0){
+		inicio();
+	printf("Escolha a operacao\n");
+	scanf("%d",&op);
+	
+	if(op == 0){
+		break;
+	}
+	else if(op == 1){
+		soma();
+	}
+	else if(op == 2){
+		sub();
+	}
+	else if(op == 3){
+		mult();
+	}
+	else if(op == 4){
+		divi();
+	}
+	else if(op == 5){
+		raiz();
+	}
+	else if(op == 6){
+		sct();
+	}
+	else if(op == 7){
+		printf("     Historico    \n");
+	}
+	else{
+		printf("Opcao Invalida!\n\n");
+	}
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	}
 	return 0;
 }
+
+
--- APS.py
+++ APS.py
@@ -1,28 +1,77 @@
 def soma():
-    n1 = int(input("Valor: "))
-    n2 = int(input(f"{n1} + "))
-    soma = n1 + n2
-    print(f"{n1} + {n2} = {soma}")
+    r=1
+    while r>0:
+        n1 = int(input("Valor: "))
+        n2 = int(input(f"{n1} + "))
+        soma = n1 + n2
+        print(f"\n{n1} + {n2} = {soma}\n")
+        r=int(input("1-Continuar 0-Sair: "))
+        if r > 1:
+            print("Opção Inválida!....Saindo")
+            r=0
 def sub():
-    n1 = int(input("Valor: "))
-    n2 = int(input(f"{n1} - "))
-    sub = n1 - n2
-    print(f"{n1} - {n2} = {sub}")
+    r = 1
+    while r > 0:
+        n1 = int(input("Valor: "))
+        n2 = int(input(f"{n1} - "))
+        sub = n1 - n2
+        print(f"\n{n1} - {n2} = {sub}\n")
+        r = int(input("1-Continuar 0-Sair: "))
+        if r > 1:
+            print("Opção Inválida!....Saindo")
+            r = 0
 def mult():
-    n1 = int(input("Valor: "))
-    n2 = int(input(f"{n1} X "))
-    mult = n1 * n2
-    print(f"{n1} X {n2} = {mult}")
+    r = 1
+    while r > 0:
+        n1 = int(input("Valor: "))
+        n2 = int(input(f"{n1} X "))
+        mult = n1 * n2
+        print(f"\n{n1} X {n2} = {mult}\n")
+        r = int(input("1-Continuar 0-Sair: "))
+        if r > 1:
+            print("Opção Inválida!....Saindo")
+            r = 0
 def div():
-    n1 = int(input("Valor: "))
-    n2 = int(input(f"{n1}/ "))
-    div = n1 / n2
-    print("{} / {} = {:.2f}".format(n1,n2,div))
-
-
-
-
-
+    r = 1
+    while r > 0:
+        n1 = int(input("Valor: "))
+        n2 = int(input(f"{n1}/ "))
+        if n2 == 0:
+            print("Não existe divisão por 0! ")
+            break
+        div = n1 / n2
+        print("\n{} / {} = {:.2f}\n".format(n1, n2, div))
+        r = int(input("1-Continuar 0-Sair: "))
+        if r > 1:
+            print("Opção Inválida!....Saindo")
+            r = 0
+def raiz():
+    import math
+    r = 1
+    while r > 0:
+        n = int(input("Digite o valor: "))
+        if n < 0:
+            print("Não existe raiz de números negativos!")
+            break
+        raiz = math.sqrt(n)
+        print("\nRaiz de {} = {:.2f}\n".format(n, raiz))
+        r = int(input("1-Continuar 0-Sair: "))
+        if r > 1:
+            print("Opção Inválida!....Saindo")
+            r = 0
+def sct():
+    import math
+    r = 1
+    while r > 0:
+        n = float(input("Digite o valor: "))
+        seno = math.sin(math.radians(n))
+        cosseno = math.cos(math.radians(n))
+        tang = math.tan(math.radians(n))
+        print("\nO valor {} possui Seno = {:.2f}, Cosseno = {:.2f} e Tangente = {:.2f}\n".format(n, seno, cosseno, tang))
+        r = int(input("1-Continuar 0-Sair: "))
+        if r > 1:
+            print("Opção Inválida!....Saindo")
+            r = 0
 print("""
 -------------CALCULADORA--------------
 -----------Pablo--Vinícius------------
@@ -36,14 +85,10 @@ while corpo == True:
     3 - Multiplicação
     4 - Divisão
     5 - Raiz Quadrada
-    6 - Seno
-    7 - Cosseno
-    8 - Tangente
+    6 - Seno, Cosseno, Tangente
     0 - Sair
     """)
-
     op = int(input("Escolha a operação: "))
-
     if op == 1:
         soma()
     elif op == 2:
@@ -53,13 +98,9 @@ while corpo == True:
     elif op == 4:
         div()
     elif op == 5:
-        print("kasjk")
+        raiz()
     elif op == 6:
-        print("kasjk")
-    elif op == 7:
-        print("kasjk")
-    elif op == 8:
-        print("kasjk")
+        sct()
     elif op == 0:
         break
     else:
